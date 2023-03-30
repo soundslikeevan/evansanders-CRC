9f1aed39ff625c9740ec72a27c683904839e1a06
Evan Sanders <soundslikeevan@gmail.com>
2023-03-30 15:08:03 +1100

    Pushing to GitHub

---
+++ modules/s3-website/outputs.tf
@@ -0,0 +1,4 @@
+output "website_endpoint" {
+  description = "The public url of this website."
+  value = "${evansanders-resume.com.s3-website-ap-southeast-2.amazonaws.com}"
+}
---
+++ modules/s3-website/variables.tf
@@ -0,0 +1,3 @@
+variable "bucket_name" {
+  description = "evansanders-resume.com"
+}
